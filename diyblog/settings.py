1ed69627bc619b2f80f23effeec4f963d1855768
Nikki1970 <sagalanikhilesh99@gmail.com>
2021-04-08 22:49:37 +0530

    Add static files

--- diyblog/settings.py
+++ diyblog/settings.py
@@ -124,8 +124,4 @@ USE_TZ = True
 STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
 
