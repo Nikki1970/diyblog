25368571cdc94966fb6ac6321e08dedc5454761a
Nikki1970 <sagalanikhilesh99@gmail.com>
2021-04-08 20:14:45 +0530

    Update settings for heroku deployment

--- diyblog/settings.py
+++ diyblog/settings.py
@@ -121,7 +121,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-PROJECT_ROOT   =   Path(__file__).resolve().parent.parent
 STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
