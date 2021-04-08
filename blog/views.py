a9d1343518d268fdea8ce146ca4fa871ad992c7d
Nikki1970 <sagalanikhilesh99@gmail.com>
2021-04-08 16:45:19 +0530

    Create index page of blog times

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from blog.models import Blog, BlogAuthor, BlogComment
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_blogs = Blog.objects.all().count()
+    num_authors = BlogAuthor.objects.all().count()
+    num_comments = BlogComment.objects.count()
+
+    context = {
+        'num_blogs': num_blogs,
+        'num_authors': num_authors,
+        'num_comments': num_comments,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
