00e4be6d280cce31786a2f35fd081917c038169c
Nikki1970 <sagalanikhilesh99@gmail.com>
2021-04-11 17:14:20 +0530

    Posting a blog by any logged user

--- blog/forms.py
+++ blog/forms.py
@@ -7,3 +7,10 @@ class BlogCommentForm(forms.ModelForm):
     class Meta:
         model = BlogComment
         fields = ('description',)
+
+class BlogForm(forms.ModelForm):
+
+    class Meta:
+        model = Blog
+        fields = ('title','detail',)
+
